515570ec1b8be5d65d65cb60df5eb0b61af645a6
Serkandgn81 <doganserkan344@gmail.com>
2025-01-28 22:44:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID", 23915346))
-API_HASH = getenv("API_HASH", "3bc48fd6db8e9c1cfdc89c8de742d188")
+API_ID = int(getenv("API_ID", 26434153))
+API_HASH = getenv("API_HASH", "b7bab20a44886d5207f7781cb6f12fe8")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "7447707289:AAGl9BitaXMeBJBtX-v22gjcj3DfMQQCA2c")
+BOT_TOKEN = getenv("BOT_TOKEN", "7665037614:AAGjLuwFKZ3kF-thARzAO-NIsSFej4aTbPE")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://erkbwrs084:909090@cluster0.qdrfgmb.mongodb.net/?retryWrites=true&w=majority")
 
 OWNER_ID = int(getenv("OWNER_ID", 7305205222))
